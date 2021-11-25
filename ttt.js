3430ad358804fb04269bc824c58010e4abba0346
Irbaaz <irbaazhussain49@gmail.com>
2021-11-25 18:43:30 +0530

    added music files and error showing in checkwin() saying e is not defind creating new branch to resolve the issue

---
+++ ttt.js
@@ -0,0 +1,49 @@
+console.log("hello");
+
+let music = new Audio('music.mp3');
+let audioTurn =new Audio("img/ting.mp3")
+let gameOver = new Audio("img/gameover.mp3");
+
+
+let turn = "X";
+
+
+const changeTurn = ()=>{
+    return turn === "X"?"0":"X"
+}
+
+const checkWin = () =>{
+    let boxtexts = document.getElementsByClassName('boxText');
+    console.log(boxtexts)
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+        [0,4,8],
+        [2,4,6],    ]
+wins.forEach(e=>{
+    let boxtexts = document.getElementsByClassName('boxText');
+    if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText)&&(boxtexts[e[2]].innerText === boxtexts[e[1]].innerText)&&(boxtexts[e[0]].innerText !== "")){
+        document.querySelector('.info').innerText = boxtexts[e[0]].innerText + "WON"
+    }
+
+})
+}
+
+let boxes =document.getElementsByClassName("box");
+
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxText');
+element.addEventListener('click', ()=>{
+    if(boxtext.innerText === ''){
+        boxtext.innerText = turn;
+        turn = changeTurn();
+        audioTurn.play();
+        checkWin();
+        document.getElementsByClassName("info")[0].innerText = 'Turn for' + turn;
+    }
+})
+})
